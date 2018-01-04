45e52b819c00e8cf133f244f95154ae72d564c39
Hagen Mohr <hagen.mohr@gmail.com>
2018-01-04 10:33:23 +0100

    finished script to work with multiple features (no GridSearch)

--- Masch_Sprachverarbeitung/EMail_Classifier/uebung3-group7_single_feature.py
+++ Masch_Sprachverarbeitung/EMail_Classifier/uebung3-group7_single_feature.py
@@ -8,24 +8,13 @@ import os
 import re
 from nltk.corpus import stopwords
 import scipy.sparse as sp
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 from sklearn.externals import joblib
-from sklearn.base import TransformerMixin
-
-class ItemSelector(TransformerMixin):
-    def __init__(self, column):
-        self.column = column
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        return X[self.column]
-
-
 
 
 
@@ -37,15 +26,14 @@ def main():
         ham_directory = sys.argv[3]
         spam_directory = sys.argv[4]
         train_model(model_name, ham_directory, spam_directory)
-        print('---- Training sucessfull ----')
-    elif sys.argv[1].lower() == 'classify':
+        print('---- Training successful ----')
+    elif sys.argv[1].lower() == 'classify'and len(sys.argv) == 5:
         model_name = sys.argv[2]
         email_directory = sys.argv[3]
         result_file = sys.argv[4]
         classify_mails(model_name, email_directory, result_file)
         print('---- Prediction completed ----')
     else:
-        print(sys.argv[1].lower())
         print('Oops, something went wrong, please check if you called the script correctly :)')
 
 def train_model(model_name, ham_directory, spam_directory):
@@ -57,45 +45,29 @@ def train_model(model_name, ham_directory, spam_directory):
     df_final = pd.concat([df_ham, df_spam])
     df_final = df_final.sample(frac=1).reset_index(drop=True)
 
-    X = df_final[['body', 'subject', 'sender']]
+    X = df_final.body.values
     y = df_final.label.values
-
-        # body pipe
-    body_pipe = make_pipeline(
-        ItemSelector('body'),
-        TfidfVectorizer(encoding='latin1', lowercase=False, ngram_range=(1,3))
-    )
-
-    # subject pipe
-    subject_pipe = make_pipeline(
-        ItemSelector('subject'),
-        TfidfVectorizer(encoding='latin1', lowercase=False, ngram_range=(1,3))
-    )
-
-    # sender pipe
-    sender_pipe = make_pipeline(
-        ItemSelector('sender'),
-        TfidfVectorizer(encoding='latin1', lowercase=False, ngram_range=(1,3))
-    )
-
-    feature_union = make_union(body_pipe, subject_pipe, sender_pipe)
-
-    lr_tfidf = Pipeline([
-        ('union', feature_union),
-        ('clf', LogisticRegression(random_state=0))
-    ])
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-    # Tune LR
+    # Since we already kfolded and tested we know that we should pick LR
+    lr_tfidf = Pipeline([
+        ('count', CountVectorizer(ngram_range=(1,2))),
+        ('vect', TfidfTransformer()),
+        ('clf', LogisticRegression(random_state=0)) # default C to 1.0, mb tune later
+    ])
+
+
     param_grid = [
-        {
-            'clf__penalty': ['l1', 'l2'],
-            'clf__C': [0.1, 1.0, 10.0, 100.0]
-        }
+    {
+        'count__ngram_range': [(1,2), (1,3)],
+        'count__lowercase': [True, False],
+        'clf__penalty': ['l1', 'l2'],
+        'clf__C': [0.1, 1.0, 10.0, 100.0]
+    }
     ]
 
-    gs_lr = GridSearchCV(
+    gs_lr_tfidf = GridSearchCV(
         lr_tfidf,
         param_grid,
         scoring='accuracy',
@@ -103,10 +75,10 @@ def train_model(model_name, ham_directory, spam_directory):
         n_jobs=-1
     )
 
-    gs_lr.fit(X_train, y_train)
-    print('Parameter: {}'.format(gs_lr.best_params_))
-    print('Accuracy (CV): {}'.format(gs_lr.best_score_))
-    best_classifier = gs_lr.best_estimator_
+    gs_lr_tfidf.fit(X_train, y_train)
+    print('Parameter: {}'.format(gs_lr_tfidf.best_params_))
+    print('Accuracy (CV): {}'.format(gs_lr_tfidf.best_score_))
+    best_classifier = gs_lr_tfidf.best_estimator_
     print('Accuracy (Test): {}'.format(best_classifier.score(X_test, y_test)))
 
     # Train on the complete corpus
@@ -116,19 +88,18 @@ def train_model(model_name, ham_directory, spam_directory):
     joblib.dump(best_classifier, '{}.clf'.format(model_name))
 
 def classify_mails(model_name, email_directory, result_file):
-    df = make_df(email_directory, 2) # label gets dropped later anyway
-    X = df[['body', 'subject', 'sender']]
+    df_test = make_df(email_directory, 2) #label doesn't matter anyway
+    X = df_test.body.values
     clf = joblib.load(model_name)
     y = clf.predict(X)
     my_list = y.tolist()
     my_list = ['SPAM' if elem == 1  else 'HAM' for elem in my_list]
     filenames = os.listdir(email_directory)
-    my_file = open(result_file, 'w')
+    file = open(result_file, 'w')
 
     for i, item in enumerate(my_list):
-        my_file.write("{} \t {}\n".format(filenames[i],item))
-    my_file.close()
-
+        file.write("{} \t {}\n".format(filenames[i],item))
+    file.close()
 
 
 
